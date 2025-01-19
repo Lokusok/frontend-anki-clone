d30cda2fbb6432fddeaa940eeafdac1e21c899c7
Lokusok <lokusokdrobk@gmail.com>
2025-01-19 19:52:05 +0300

    Add skeleton

---
+++ src/app/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import useCounterStore from '../stores/counter';
+
+const counterStore = useCounterStore();
+</script>
+
+<template>
+  <h1>Vue 3</h1>
+
+  <h3>{{ counterStore.count }} | {{ counterStore.doubleCount }}</h3>
+
+  <button @click="counterStore.increment">Increment</button>
+
+  <RouterView />
+</template>
---
+++ src/stores/counter.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia';
+
+const useCounterStore = defineStore('counter', {
+    state: () => ({
+        count: 0
+    }),
+    getters: {
+        doubleCount: (state) => state.count ** 2
+    },
+    actions: {
+        increment() {
+            this.count++;
+        }
+    }
+});
+
+export default useCounterStore;
