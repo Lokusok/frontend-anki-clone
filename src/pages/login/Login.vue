fb7dc32f8601fc1853e2ee85cde2f13e5746e670
Lokusok <lokusokdrobk@gmail.com>
2025-01-20 17:37:26 +0300

    Add pages login and register

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
 import PageLayout from '../../components/layouts/PageLayout.vue';
 
+const loginUser = () => {
+
+};
 </script>
 
 <template>
-    <PageLayout title="Войти в аккаунт"></PageLayout>
+    <PageLayout title="Войти в аккаунт">
+        <v-sheet
+            :style="{ 'max-width': '1024px', 'margin': '0 auto' }"
+            elevation="10"
+            class="pa-3 rounded-lg"
+        >
+            <form @submit.prevent="loginUser">
+                <v-text-field label="Почта" />
+                <v-text-field label="Пароль" />
+
+                <div class="d-flex justify-space-between align-center">
+                    <v-btn type="submit" color="primary">Войти</v-btn>
+                    <router-link :to="{ name: 'register' }">Нет аккуанта?</router-link>
+                </div>
+            </form>
+        </v-sheet>
+    </PageLayout>
 </template>
--- src/pages/register/Register.vue
+++ src/pages/register/Register.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
 import PageLayout from '../../components/layouts/PageLayout.vue';
 
+const registerUser = () => {
+
+};
 </script>
 
 <template>
-    <PageLayout title="Регистрация"></PageLayout>
+    <PageLayout title="Регистрация">
+        <v-sheet
+            :style="{ 'max-width': '1024px', 'margin': '0 auto' }"
+            elevation="10"
+            class="pa-3 rounded-lg"
+        >
+            <form @submit.prevent="registerUser">
+                <v-text-field label="Логин" />
+                <v-text-field label="Почта" />
+                <v-text-field label="Пароль" />
+                <v-text-field label="Подтвердите пароль" />
+
+                <div class="d-flex justify-space-between align-center">
+                    <v-btn type="submit" color="primary">Создать аккаунт</v-btn>
+                    <router-link :to="{ name: 'login' }">Уже есть аккаунт?</router-link>
+                </div>
+            </form>
+        </v-sheet>
+    </PageLayout>
 </template>
