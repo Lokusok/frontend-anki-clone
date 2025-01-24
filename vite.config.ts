cfd8da4f8a631adeaa70e227350dfbb03c4bbe37
Lokusok <lokusokdrobk@gmail.com>
2025-01-24 21:36:53 +0300

    A little optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 600
   }
 })
